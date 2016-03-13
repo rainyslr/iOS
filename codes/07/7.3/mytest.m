bf8c2c3ed58348adf13ec45a52d217a94841f88d
rainySue <410692979@qq.com>
2016-03-13 17:04:32 +0800

    add code in crazy iOS

---
+++ codes/07/7.3/mytest.m
@@ -0,0 +1,28 @@
+#import <Foundation/Foundation.h>
+
+// @interface FKDog : NSObject <NSCopying>
+// @property (nonatomic , strong) NSMutableString* name;
+// @property (nonatomic , assign) int age;
+// @end
+
+
+int main(int argc , char * argv[])
+{
+	@autoreleasepool{
+		// 创建一个FKDog对象
+// 		FKDog* dog1 = [FKDog new];
+// 		dog1.name = [NSMutableString stringWithString:@"旺财"];
+// 		dog1.age = 20;
+// 		// 复制副本
+// 		FKDog* dog2 = [dog1 copy];
+// 		// 复制对象的可变副本
+// //		FKDog* dog2 = [dog1 mutableCopy];
+// 		dog2.name = [NSMutableString stringWithString:@"snoopy"];
+// 		dog2.age = 12;
+// 		NSLog(@"dog1的名字为：%@" , dog1.name);
+// 		NSLog(@"dog1的年龄为：%d" , dog1.age);
+// 		NSLog(@"dog2的名字为：%@" , dog2.name);
+// 		NSLog(@"dog2的年龄为：%d" , dog2.age);
+		NSLog(@"%@",@"hello");
+	}
+}
